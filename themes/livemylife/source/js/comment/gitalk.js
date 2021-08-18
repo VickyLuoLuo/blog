5a13e7b155a1a8e806c25c3509f170629af3a9d4
luohongli <15754300540@163.com>
2021-08-18 23:22:20 +0800

    update

--- themes/livemylife/source/js/comment/gitalk.js
+++ themes/livemylife/source/js/comment/gitalk.js
@@ -7280,7 +7280,7 @@
                             pagerDirection: 'last', // last or first
                             createIssueManually: false,
                             distractionFreeMode: false,
-                            proxy: 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token',
+                            proxy: 'https://cors.bridged.cc/https://github.com/login/oauth/access_token',
                             flipMoveOptions: {
                                 staggerDelayBy: 150,
                                 appearAnimation: 'accordionVertical',
